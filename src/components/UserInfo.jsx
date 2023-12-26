46e021c352b343ea9694b798595da7cc8f8ee3b9
Md. Anisur Rahman Rony <anisur.rony@technonext.com>
2023-12-26 15:00:29 +0600

    update sign out functionality

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,19 +1,21 @@
-'use client';
+"use client";
 
-import { signOut } from 'next-auth/react';
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const UserInfo = ({ user }) => {
+  const router = useRouter();
   const handleLogout = async () => {
-    await signOut();
+    await signOut({ callbackUrl: "/" });
   };
 
   return (
-    <div className='rounded-lg border p-10 shadow-lg'>
+    <div className="rounded-lg border p-10 shadow-lg">
       <div>Id : {user.id}</div>
       <div>Name : {user.name}</div>
       <div>Email : {user.email}</div>
       <button
-        className='mt-2 font-medium text-blue-600 hover:underline'
+        className="mt-2 font-medium text-blue-600 hover:underline"
         onClick={handleLogout}
       >
         Log out
