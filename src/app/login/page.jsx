c38696dc1bfb61635abe32ec2990287d35162874
Md. Anisur Rahman Rony <anisur.rony@technonext.com>
2023-12-26 14:52:47 +0600

    fix bug

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,17 +1,17 @@
-import UserInfo from '@/components/UserInfo';
-import Link from 'next/link';
-import { getServerAuthSession } from '../server/auth';
+import UserInfo from "@/components/UserInfo";
+import Link from "next/link";
+import { getServerAuthSession } from "../server/auth";
 
 export default async function HomePage() {
   const authSession = await getServerAuthSession();
 
   return (
-    <main className='flex h-screen items-center justify-center'>
+    <main className="flex h-screen items-center justify-center">
       {authSession?.user && <UserInfo user={authSession?.user} />}
       {!authSession?.user && (
         <Link
-          className='mt-2 font-medium text-blue-600 hover:underline'
-          href='/login'
+          className="mt-2 font-medium text-blue-600 hover:underline"
+          href="/"
         >
           Login
         </Link>
